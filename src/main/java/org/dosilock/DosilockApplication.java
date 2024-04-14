377b7e59f799478ad0f77058c3e5ed8e9f281418
heroq <96118954+heroq@users.noreply.github.com>
2024-04-14 10:06:50 +0900

    Feat: init

---
+++ src/main/java/org/dosilock/DosilockApplication.java
@@ -0,0 +1,13 @@
+package org.dosilock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DosilockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DosilockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dosilock/DosilockApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dosilock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DosilockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
