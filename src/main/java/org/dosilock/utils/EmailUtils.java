845222255dc6d91a863995f28ddfc936f85596be
heroq <96118954+heroq@users.noreply.github.com>
2024-04-21 17:14:55 +0900

    Refactor: 이메일 분리

--- src/main/java/org/dosilock/utils/EmailUtils.java
+++ src/main/java/org/dosilock/utils/EmailUtils.java
@@ -11,13 +11,21 @@ import lombok.RequiredArgsConstructor;
 public class EmailUtils {
 	private final JavaMailSender emailSender;
 
-	public void sendSimpleMessage(
-		String to, String subject, String text) {
+	public void sendSimpleMessage(String to, String subject, String text) {
 		SimpleMailMessage message = new SimpleMailMessage();
 		message.setFrom("gongsilock@gmail.com");
 		message.setTo(to);
 		message.setSubject(subject);
 		message.setText(text);
+		emailSender.send(message);
+	}
+
+	public void sendSingupMessage(String to) {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setFrom("gongsilock@gmail.com");
+		message.setTo(to);
+		message.setSubject("공시락 회원가입 이메일 인증 입니다.");
+		message.setText("확인 버튼");
 		emailSender.send(message);
 	}
 }
