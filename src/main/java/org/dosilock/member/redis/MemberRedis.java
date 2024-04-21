361b69ea38a21c416d3808cf74dd8b7a5a6a7bd6
heroq <96118954+heroq@users.noreply.github.com>
2024-04-21 17:14:35 +0900

    Feat: 회원가입 이메일 인증 서비스 로직 추가

---
+++ src/main/java/org/dosilock/member/redis/MemberRedis.java
@@ -0,0 +1,20 @@
+package org.dosilock.member.redis;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@RedisHash(value = "member", timeToLive = 900)
+@Builder
+@Getter
+public class MemberRedis {
+	@Id
+	private String email;
+	private String userData;
+	@Indexed
+	private String link;
+	private Boolean isEmailVerified;
+}
