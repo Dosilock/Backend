e60260fa5b9b09e70e1ce9c42e7c4e1cc1fd3c31
heroq <96118954+heroq@users.noreply.github.com>
2024-04-22 18:29:04 +0900

    Feat: OAuth2 설정

--- src/main/java/org/dosilock/configuration/SecurityConfig.java
+++ src/main/java/org/dosilock/configuration/SecurityConfig.java
@@ -2,6 +2,7 @@ package org.dosilock.configuration;
 
 import org.dosilock.jwt.JwtAuthenticationFilter;
 import org.dosilock.jwt.JwtTokenProvider;
+import org.dosilock.oauth.OAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -22,6 +23,7 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig {
 
 	private final JwtTokenProvider jwtTokenProvider;
+	private final OAuth2UserService oAuth2UserService;
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
@@ -33,7 +35,12 @@ public class SecurityConfig {
 				authorize
 					.anyRequest()
 					.permitAll())
-
+			.oauth2Login(oauth2 -> oauth2
+				.loginPage("/login") // 로그인 페이지 경로 설정 (선택 사항)
+				.userInfoEndpoint(userInfo -> userInfo
+					.userService(oAuth2UserService) // OAuth2UserService를 구현한 커스텀 서비스
+				)
+			)
 			.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
 		return httpSecurity.build();
 
