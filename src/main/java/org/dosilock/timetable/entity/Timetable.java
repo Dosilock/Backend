c7747dcc4d6f2a068342b974eedf59a9ed8c8141
heroq <96118954+heroq@users.noreply.github.com>
2024-05-04 14:37:27 +0900

    Refacotr: timetable entity에 day 추가

--- src/main/java/org/dosilock/timetable/entity/Timetable.java
+++ src/main/java/org/dosilock/timetable/entity/Timetable.java
@@ -3,13 +3,17 @@ package org.dosilock.timetable.entity;
 import java.time.LocalDateTime;
 
 import org.dosilock.clazz.entity.Clazz;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,21 +23,29 @@ import lombok.Setter;
 
 @Entity
 @Table(name = "timetable")
-@Getter @Setter
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@EntityListeners(AuditingEntityListener.class)
 public class Timetable {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@OneToOne
+	@ManyToOne
 	@JoinColumn(name = "clazz_id", referencedColumnName = "id")
 	private Clazz clazz;
 
+	@Column
+	private Day timetableDay;
+
+	@Column
 	private String timetableName;
 
+	@CreatedDate
+	@Column(nullable = false, updatable = false)
 	private LocalDateTime createdAt;
 }
