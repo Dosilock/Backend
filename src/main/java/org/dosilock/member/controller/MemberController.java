967d59a31619e3234aa0d7c6a757709016673d57
heroq <96118954+heroq@users.noreply.github.com>
2024-05-05 17:02:29 +0900

    Refactor: Member 스웨거 설명 추가

--- src/main/java/org/dosilock/member/controller/MemberController.java
+++ src/main/java/org/dosilock/member/controller/MemberController.java
@@ -6,6 +6,7 @@ import org.dosilock.member.request.RequestMemberDto;
 import org.dosilock.member.response.ResponseMemberDto;
 import org.dosilock.member.service.v1.MemberService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -21,11 +23,13 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/")
+@CrossOrigin
 public class MemberController {
 
 	private final MemberService memberService;
 	private final JwtTokenProvider jwtTokenProvider;
 
+	@Operation(summary = "로그인 API", description = "이메일 로그인")
 	@PostMapping(value = "signin")
 	public ResponseEntity<Void> signin(HttpServletResponse httpServletResponse,
 		@RequestBody RequestMemberDto requestMemberDto) {
@@ -34,29 +38,34 @@ public class MemberController {
 		return ResponseEntity.ok().build();
 	}
 
+	@Operation(summary = "회원가입 API", description = "이메일 회원가입")
 	@PostMapping(value = "signup")
 	public ResponseEntity<Void> signup(@Valid @RequestBody RequestMemberDto requestMemberDto) {
 		memberService.signup(requestMemberDto);
 		return ResponseEntity.ok().build();
 	}
 
+	@Operation(summary = "회원가입 승인 이메일 전달 API", description = "이메일 회원가입 승인 이메일 전달")
 	@GetMapping(value = "signup/link/{linkCode}")
 	public ResponseEntity<Void> confirmEmailVerification(@PathVariable String linkCode) {
 		memberService.confirmEmailVerification(linkCode);
 		return ResponseEntity.ok().build();
 	}
 
+	@Operation(summary = "내 정보 API", description = "자신의 정보 확인")
 	@PostMapping(value = "mypage")
 	public ResponseEntity<ResponseMemberDto> myPage() {
 		return ResponseEntity.ok(memberService.myPage());
 	}
 
+	@Operation(summary = "비밀번호 변경 승인 이메일 요청 API", description = "비밀번호 변경을 위한 이메일 승인 요청")
 	@PostMapping(value = "password")
 	public ResponseEntity<Void> changePassword(@RequestBody RequestMemberDto requestMemberDto) {
 		memberService.changePassword(requestMemberDto);
 		return ResponseEntity.ok().build();
 	}
 
+	@Operation(summary = "비밀번호 변경 이메일 요청 API", description = "비밀번호 변경 이메일 승인 요청")
 	@PutMapping(value = "password/link/{linkCode}")
 	public ResponseEntity<Void> confirmChangePassword(@PathVariable String linkCode) {
 		memberService.confirmChangePassword(linkCode);
