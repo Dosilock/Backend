da7be047146957480fe5fbd71c08f4f17e1d432d
heroq <96118954+heroq@users.noreply.github.com>
2024-05-06 17:55:12 +0900

    Feat: 시간표 보기 추가

--- src/main/java/org/dosilock/timetable/controller/TimetableController.java
+++ src/main/java/org/dosilock/timetable/controller/TimetableController.java
@@ -1,9 +1,11 @@
 package org.dosilock.timetable.controller;
 
+import org.dosilock.timetable.response.TimetableResponse;
 import org.dosilock.timetable.service.TimetableService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,11 +21,10 @@ public class TimetableController {
 
 	private final TimetableService timetableService;
 
-	@Operation(summary = "시간표 보기 API", description = "")
-	@GetMapping(value = "info")
-	public ResponseEntity<Object> getTimetableInfo() {
-		
-		return ResponseEntity.ok().build();
+	@Operation(summary = "시간표 보기 API", description = "시간표의 정보와 시간표 모든 교시를 보여줍니다.")
+	@GetMapping(value = "info/{timetableId}")
+	public ResponseEntity<TimetableResponse> getTimetableInfo(@PathVariable Long timetableId) {
+		return ResponseEntity.ok(timetableService.getTimetableInfo(timetableId));
 	}
 
 	@Operation(summary = "시간표 관리 API", description = "")
--- src/main/java/org/dosilock/timetable/service/TimetableService.java
+++ src/main/java/org/dosilock/timetable/service/TimetableService.java
@@ -20,6 +20,8 @@ public class TimetableService {
 
 	public TimetableResponse getTimetableInfo(Long timetableId) {
 
+		// 자신이 해당 시간표를 생성한 반 인원에 속해 있어야지 보여 줄 수 있어야함.
+
 		Timetable timetable = timetableRepository.findById(timetableId)
 			.orElseThrow(() -> new NullPointerException("없는 시간표 입니다."));
 
