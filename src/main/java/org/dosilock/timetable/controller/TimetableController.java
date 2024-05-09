8b661cb29f6ae125c040fe5389babff5c83dc601
heroq <96118954+heroq@users.noreply.github.com>
2024-05-09 16:31:17 +0900

    Feat: Swagger 시간표 요청, 응답 설명 추가 (#18)

--- src/main/java/org/dosilock/timetable/controller/TimetableController.java
+++ src/main/java/org/dosilock/timetable/controller/TimetableController.java
@@ -7,13 +7,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -24,36 +26,32 @@ public class TimetableController {
 	private final TimetableService timetableService;
 
 	@Operation(summary = "시간표 보기 API", description = "시간표의 정보와 시간표 모든 교시를 보여줍니다.")
+	@ApiResponses({
+		@ApiResponse(responseCode = "200", description = "성공")
+	})
 	@GetMapping(value = "info/{timetableId}")
-	public ResponseEntity<TimetableResponse> getTimetableInfo(@PathVariable Long timetableId) {
+	public ResponseEntity<TimetableResponse> getTimetableInfo(
+		@Parameter(description = "시간표 아이디") @PathVariable Long timetableId) {
 		return ResponseEntity.ok(timetableService.getTimetableInfo(timetableId));
 	}
 
-	@Operation(summary = "시간표 관리 API", description = "")
-	@PostMapping(value = "manage")
-	public ResponseEntity<Object> getTimetableManage() {
-
-		return ResponseEntity.ok().build();
-	}
-
-	@Operation(summary = "시간표 생성 API", description = "")
-	@PostMapping()
-	public ResponseEntity<Object> getTimetalbeCreated() {
-
-		return ResponseEntity.ok().build();
-	}
-
 	@Operation(summary = "시간표 편집 API", description = "시간표를 편집 합니다.")
+	@ApiResponses({
+		@ApiResponse(responseCode = "200", description = "성공")
+	})
 	@PutMapping(value = "modify/{timetableId}")
-	public ResponseEntity<Object> updateTimetable(@RequestBody TimetableRequest timetableRequest,
-		@PathVariable Long timetableId) {
+	public ResponseEntity<Void> updateTimetable(@RequestBody TimetableRequest timetableRequest,
+		@Parameter(description = "시간표 아이디") @PathVariable Long timetableId) {
 		timetableService.updateTimetable(timetableRequest, timetableId);
 		return ResponseEntity.ok().build();
 	}
 
 	@Operation(summary = "시간표 삭제 API", description = "시간표를 삭제 합니다.")
+	@ApiResponses({
+		@ApiResponse(responseCode = "200", description = "성공")
+	})
 	@DeleteMapping(value = "delete/{timetableId}")
-	public ResponseEntity<Void> deleteTimetable(@PathVariable Long timetableId) {
+	public ResponseEntity<Void> deleteTimetable(@Parameter(description = "시간표 아이디") @PathVariable Long timetableId) {
 		timetableService.deleteTimetable(timetableId);
 		return ResponseEntity.ok().build();
 	}
--- src/main/java/org/dosilock/timetable/request/TimetableRequest.java
+++ src/main/java/org/dosilock/timetable/request/TimetableRequest.java
@@ -2,6 +2,7 @@ package org.dosilock.timetable.request;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,8 +14,14 @@ import lombok.Setter;
 @AllArgsConstructor
 @Builder
 public class TimetableRequest {
+
+	@Schema(description = "시간표 이름")
 	@Size(min = 2, max = 50, message = "50자를 넘길 수 없음..")
 	private String timetableName;
+
+	@Schema(description = "시간표 일수")
 	private List<Integer> timetableDays;
+
+	@Schema(description = "교시")
 	private List<PeriodRequest> periodRequests;
 }
