e08f24c320d94b24185dcc2cce699a6bdd62d8e6
황영웅 <96118954+heroq@users.noreply.github.com>
2024-05-21 19:00:16 +0900

    :recycle: 기존 응답 코드 제거 (#38) (#39)

--- src/main/java/org/dosilock/configuration/login/CustomAuthenticationSuccessHandler.java
+++ src/main/java/org/dosilock/configuration/login/CustomAuthenticationSuccessHandler.java
@@ -1,15 +1,9 @@
 package org.dosilock.configuration.login;
 
-import java.io.IOException;
-
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -19,19 +13,11 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
-	private ObjectMapper objectMapper = new ObjectMapper();
-
 	@Override
-	public void onAuthenticationSuccess(HttpServletRequest request,
-		HttpServletResponse response,
-		Authentication authentication) throws IOException {
-
-		User user = (User)authentication.getPrincipal();
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+		Authentication authentication) {
 
 		response.setStatus(HttpStatus.OK.value());
-		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-
-		objectMapper.writeValue(response.getWriter(), user);
 	}
 }
 
