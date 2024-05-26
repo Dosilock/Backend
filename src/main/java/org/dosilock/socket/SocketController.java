756d50c0b03fd460aa89314e342d467b8803d950
heroq <96118954+heroq@users.noreply.github.com>
2024-05-26 18:22:16 +0900

    :sparkles: 세션 아이디 가져오기 추가 (#42)

--- src/main/java/org/dosilock/socket/SocketController.java
+++ src/main/java/org/dosilock/socket/SocketController.java
@@ -1,11 +1,16 @@
 package org.dosilock.socket;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -15,9 +20,16 @@ public class SocketController {
 
 	private final SocketService socketService;
 
-	@PostMapping("/{name}")
+	@PostMapping("{name}")
 	public ResponseEntity<Void> createNamespace(@PathVariable("name") String name) {
 		socketService.addNamespace(name);
 		return ResponseEntity.ok().build();
+	}
+
+	@GetMapping("sessionid")
+	public ResponseEntity<Map<String, String>> getCookie(HttpServletRequest request) {
+		Map<String, String> cookie = new HashMap<>();
+		cookie.put("sessionId", request.getSession().getId());
+		return ResponseEntity.ok(cookie);
 	}
 }
