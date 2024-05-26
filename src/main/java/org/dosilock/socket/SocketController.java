cb45bfb1c0ddf1cebfe88e78a7e163247e401a70
ARProxy <hocci0222@kakao.com>
2024-05-26 18:19:48 +0900

    ♻️ ServerTime, 서버 시간 API 응답 추가
    (#42)

--- src/main/java/org/dosilock/socket/SocketController.java
+++ src/main/java/org/dosilock/socket/SocketController.java
@@ -1,6 +1,12 @@
 package org.dosilock.socket;
 
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,14 +16,22 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/socket/")
+@RequestMapping("/api/v1/socket")
 public class SocketController {
 
 	private final SocketService socketService;
 
-	@PostMapping("/{name}")
+	@PostMapping(value = "/{name}")
 	public ResponseEntity<Void> createNamespace(@PathVariable("name") String name) {
 		socketService.addNamespace(name);
 		return ResponseEntity.ok().build();
+	}
+
+	@GetMapping(value = "/server/time")
+	public ResponseEntity<Map<String, String>> getServerTime() {
+		Map<String, String> response = new HashMap<>();
+		String serverTime = ZonedDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
+		response.put("serverTime", serverTime);
+		return ResponseEntity.ok(response);
 	}
 }
